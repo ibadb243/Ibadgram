14aeadd1b7b7c48029891b9668d2b0f005da65e6
IbadDesktop <kronosxplay243@gmail.com>
2025-05-09 10:57:36 +0400

    feat: add refresh token

---
+++ Domain/Entities/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RefreshToken : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public string Info { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiresAtUtc { get; set; }
+        public User User { get; set; }
+    }
+}
