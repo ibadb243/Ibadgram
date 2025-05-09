ec0132c6a5798b406928f56d9e28133ddfaab53f
IbadDesktop <kronosxplay243@gmail.com>
2025-05-09 11:37:45 +0400

    feat: add mention

---
+++ Domain/Common/Mention.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public abstract class Mention : BaseEntity
+    {
+        public string Shortname { get; set; } = string.Empty;
+    }
+}
---
+++ Domain/Entities/UserMention.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UserMention : Mention
+    {
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
