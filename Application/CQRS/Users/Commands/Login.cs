3a47507a20cb57c22980f566649d756268d6c17f
IbadDesktop <kronosxplay243@gmail.com>
2025-05-10 14:09:34 +0400

    feat: add login function

---
+++ Application/CQRS/Users/Commands/Login.cs
@@ -0,0 +1,83 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.Users.Commands.Login
+{
+    public class TokenResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+
+    public class LoginUserCommand : IRequest<TokenResponse>
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+    {
+        public LoginUserCommandValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8).MaximumLength(64);
+        }
+    }
+
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, TokenResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ITokenService _tokenService;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public LoginUserCommandHandler(
+            IUserRepository userRepository,
+            IRefreshTokenRepository refreshTokenRepository,
+            IUnitOfWork unitOfWork,
+            ITokenService tokenService,
+            IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _refreshTokenRepository = refreshTokenRepository;
+            _unitOfWork = unitOfWork;
+            _tokenService = tokenService;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<TokenResponse> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.Email, cancellationToken);
+            if (user == null) throw new Exception("Invalid email or password!");
+
+            if (!_passwordHasher.VerifyPassword(request.Password, user.PasswordHash))
+                throw new Exception("Invalid email or password!");
+
+            var at = _tokenService.GenerateAccessToken(user);
+
+            var refresh_token = new RefreshToken
+            {
+                UserId = user.Id,
+                AccessToken = at,
+                Token = _tokenService.GenerateRefreshToken(at),
+                ExpiresAtUtc = DateTime.UtcNow.AddDays(6),
+                User = user
+            };
+
+            await _refreshTokenRepository.AddAsync(refresh_token, cancellationToken);
+
+            await _unitOfWork.SaveAsync(cancellationToken);
+
+            return new TokenResponse { AccessToken = at, RefreshToken = refresh_token.Token };
+        }
+    }
+}
