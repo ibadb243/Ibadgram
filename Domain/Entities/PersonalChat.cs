500ed661049bc95512a82f656d5d57653efe0f2d
IbadDesktop <kronosxplay243@gmail.com>
2025-05-13 12:57:32 +0400

    feat(domain): add group and personal chats

---
+++ Domain/Entities/PersonalChat.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class PersonalChat : BaseChat
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
