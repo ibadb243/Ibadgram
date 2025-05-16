ee5320d39fd1a7ca0eefe2312ba3b4e9ef03d3c0
IbadDesktop <kronosxplay243@gmail.com>
2025-05-16 15:46:18 +0400

    feat: add update user and delete group functions

---
+++ Application/CQRS/Chats/Commands/DeleteGroup.cs
@@ -0,0 +1,72 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Enums;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.Chats.Commands.DeleteGroup
+{
+    public class DeleteGroupCommand : IRequest<Unit>
+    {
+        public Guid UserId { get; set; }
+        public Guid GroupId { get; set; }
+    }
+
+    public class DeleteGroupCommandValidator : AbstractValidator<DeleteGroupCommand>
+    {
+        public DeleteGroupCommandValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty();
+            RuleFor(x => x.GroupId).NotEmpty();
+        }
+    }
+
+    public class DeleteGroupCommandHandler : IRequestHandler<DeleteGroupCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IChatRepository _chatRepository;
+        private readonly IChatMemberRepository _chatMemberRepository;
+        private readonly IMentionRepository _mentionRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteGroupCommandHandler(
+            IUserRepository userRepository,
+            IChatRepository chatRepository,
+            IChatMemberRepository chatMemberRepository,
+            IMentionRepository mentionRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _chatRepository = chatRepository;
+            _chatMemberRepository = chatMemberRepository;
+            _mentionRepository = mentionRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Unit> Handle(DeleteGroupCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+            if (user == null) throw new Exception("User not found");
+
+            var group = await _chatRepository.GetByIdAsync(request.GroupId, cancellationToken);
+            if (group == null) throw new Exception("Group not found");
+
+            var member = await _chatMemberRepository.GetByIdsAsync(group.Id, user.Id, cancellationToken);
+            if (member == null) throw new Exception("You are member of group");
+
+            if (member.Role != ChatRole.Creator) throw new Exception("You are not creator of group");
+
+            await _chatRepository.DeleteAsync(group, cancellationToken);
+
+            if (!group.IsPrivate.Value) await _mentionRepository.DeleteAsync(group.Mention);
+
+            await _unitOfWork.SaveAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Application/CQRS/Users/Commands/UpdateUser.cs
@@ -0,0 +1,87 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.Users.Commands.UpdateUser
+{
+    public class UserVm
+    {
+        public string Fullname { get; set; }
+        public string Shortname { get; set; }
+    }
+
+    public class UpdateUserCommand : IRequest<UserVm>
+    {
+        public Guid UserId { get; set; }
+        public string Fullname { get; set; }
+        public string Shortname { get; set; }
+    }
+
+    public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserCommandValidator()
+        {
+            RuleFor(x => x.UserId).NotEmpty();
+            RuleFor(x => x.Fullname).NotEmpty();
+            RuleFor(x => x.Shortname).NotEmpty();
+        }
+    }
+
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserVm>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMentionRepository _mentionRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateUserCommandHandler(
+            IUserRepository userRepository,
+            IMentionRepository mentionRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _mentionRepository = mentionRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<UserVm> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.UserId, cancellationToken);
+            if (user == null) throw new Exception("User not found");
+
+            user.Fullname = request.Fullname;
+
+            if (user.Mention.Shortname != request.Shortname)
+            {
+                var mention = await _mentionRepository.GetByShortnameAsync(request.Shortname, cancellationToken);
+                if (mention != null) throw new Exception("Shortname had taken");
+
+                await _mentionRepository.DeleteAsync(user.Mention, cancellationToken);
+
+                mention = new UserMention
+                {
+                    Shortname = request.Shortname,
+                    UserId = user.Id,
+                    User = user,
+                };
+
+                await _mentionRepository.AddAsync(mention, cancellationToken);
+            }
+
+            await _userRepository.UpdateAsync(user, cancellationToken);
+
+            await _unitOfWork.SaveAsync(cancellationToken);
+
+            return new UserVm
+            {
+                Fullname = request.Fullname,
+                Shortname = request.Shortname,
+            };
+        }
+    }
+}
