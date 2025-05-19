b4f47ee3e82639a3218c2f89ece3302859a3f77e
IbadDesktop <kronosxplay243@gmail.com>
2025-05-19 19:22:31 +0400

    feat: add WebAPI layer

---
+++ WebAPI/Program.cs
@@ -0,0 +1,18 @@
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Application;
+using Persistence;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication();
+            services.AddPersistence(_configuration);
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
