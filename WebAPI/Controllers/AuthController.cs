99c490032156710710a73f07fd74cb46579e4d55
IbadDesktop <kronosxplay243@gmail.com>
2025-05-20 17:23:01 +0400

    feat(api): add auth and user controllers

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿using Application.CQRS.Users.Commands.Login;
+using Application.CQRS.Users.Commands.Refresh;
+using Application.CQRS.Users.Commands.Register;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/auth/")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register/")]
+        public async Task<IActionResult> RegisterAsync(
+            CancellationToken cancellationToken,
+            [FromBody] string? fullname,
+            [FromBody] string? email,
+            [FromBody] string? shortname,
+            [FromBody] string? password)
+        {
+            var command = new RegisterUserCommand
+            {
+                Fullname = fullname,
+                Email = email,
+                Shortname = shortname,
+                Password = password
+            };
+            
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpPost("login/")]
+        public async Task<IActionResult> LoginAsync(
+            CancellationToken cancellationToken,
+            [FromBody] string? email,
+            [FromBody] string? password)
+        {
+            var command = new LoginUserCommand
+            {
+                Email = email,
+                Password = password
+            };
+
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpPost("refresh/")]
+        public async Task<IActionResult> RefreshTokenAsync(
+            CancellationToken cancellationToken,
+            [FromBody] string? refresh_token)
+        {
+            var command = new RefreshTokenCommand
+            {
+                RefreshToken = refresh_token
+            };
+
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,93 @@
+﻿using Application.CQRS.Users.Commands.UpdateUser;
+using Application.CQRS.Users.Queries;
+using Application.CQRS.Users.Queries.Get;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/users/")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("me/")]
+        public async Task<IActionResult> GetMe(
+            CancellationToken cancellationToken)
+        {
+            var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid);
+            if (userId == null) return Unauthorized();
+
+            var query = new GetUserQuery
+            {
+                UserId = Guid.Parse(userId.Value)
+            };
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{userId:guid}")]
+        public async Task<IActionResult> Get(
+            CancellationToken cancellationToken,
+            [FromQuery] string userId)
+        {
+            var query = new GetUserQuery
+            {
+                UserId = Guid.Parse(userId)
+            };
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpGet("memberships/")]
+        public async Task<IActionResult> GetMemberships(
+            CancellationToken cancellationToken)
+        {
+            var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid);
+            if (userId == null) return Unauthorized();
+
+            var query = new GetUserMembershipsQuery
+            {
+                UserId = Guid.Parse(userId.Value)
+            };
+
+            var result = await _mediator.Send(query, cancellationToken);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(
+            CancellationToken cancellationToken,
+            [FromBody] string? fullname,
+            [FromBody] string? shortname)
+        {
+            var userId = HttpContext.User.FindFirst(JwtRegisteredClaimNames.Sid);
+            if (userId == null) return Unauthorized();
+
+            var command = new UpdateUserCommand
+            {
+                UserId = Guid.Parse(userId.Value),
+                Fullname = fullname,
+                Shortname = shortname,
+            };
+
+            var result = await _mediator.Send(command, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
