7452f893b0452b628995c02fa7b40fb44b62b18e
IbadDesktop <kronosxplay243@gmail.com>
2025-06-10 12:45:02 +0400

    feat: add funcs in unit of work

--- Application/Interfaces/Repositories/IUnitOfWork.cs
+++ Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -8,6 +8,16 @@ namespace Application.Interfaces.Repositories
 {
     public interface IUnitOfWork
     {
-        public Task SaveAsync(CancellationToken cancellationToken = default);
+        IUserRepository UserRepository { get; }
+        IRefreshTokenRepository RefreshTokenRepository { get; }
+        IMentionRepository MentionRepository { get; }
+        IUserMentionRepository UserMentionRepository { get; }
+        IChatRepository ChatRepository { get; }
+        IChatMemberRepository ChatMemberRepository { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+        Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
     }
 }
