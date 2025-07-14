5745736295e24647ff34a01662f94e09ec406e3f
IbadDesktop <kronosxplay243@gmail.com>
2025-07-14 22:14:09 +0400

    refactor: update shortname user and user to new schema

--- Application/CQRS/Users/Commands/UpdateShortname.cs
+++ Application/CQRS/Users/Commands/UpdateShortname.cs
@@ -4,6 +4,7 @@ using Domain.Entities;
 using FluentResults;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,17 +33,21 @@ namespace Application.CQRS.Users.Commands.UpdateShortname
 
             RuleFor(x => x.UserId)
                 .NotEmpty()
-                .MustAsync(BeExist)
-                .WithMessage("User not found")
-                .MustAsync(BeVerified)
-                .WithMessage("User do not pass registration");
+                    .WithMessage("UserId is required");
+                //.MustAsync(BeExist)
+                //.WithMessage("User not found")
+                //.MustAsync(BeVerified)
+                //.WithMessage("User do not pass registration");
 
             RuleFor(x => x.Shortname)
                 .NotEmpty()
+                    .WithMessage("Shortname is required")
                 .MinimumLength(ShortnameConstants.MinLength)
+                    .WithMessage($"Shortname's length should have minimum {ShortnameConstants.MinLength} characters")
                 .MaximumLength(ShortnameConstants.MaxLength)
-                .MustAsync(BeFree)
-                .WithMessage("Shortname has already taken");
+                    .WithMessage($"Shortname's length cann't have characters greater than {ShortnameConstants.MaxLength}");
+            //.MustAsync(BeFree)
+            //.WithMessage("Shortname has already taken");
         }
 
         private async Task<bool> BeExist(Guid userId, CancellationToken cancellationToken)
@@ -65,40 +70,93 @@ namespace Application.CQRS.Users.Commands.UpdateShortname
     public class UpdateShortnameCommandHandler : IRequestHandler<UpdateShortnameCommand, Result>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<UpdateShortnameCommandHandler> _logger;
 
-        public UpdateShortnameCommandHandler(IUnitOfWork unitOfWork)
+        public UpdateShortnameCommandHandler(
+            IUnitOfWork unitOfWork,
+            ILogger<UpdateShortnameCommandHandler> logger)
         {
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
 
         public async Task<Result> Handle(UpdateShortnameCommand request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Starting update shortname proccess");
+
             await _unitOfWork.BeginTransactionAsync(cancellationToken);
 
             try
             {
+                _logger.LogDebug("Retrieving user by id {UserId} from database", request.UserId);
                 var user = await _unitOfWork.UserRepository.GetByIdAsync(request.UserId, cancellationToken);
-                if (user == null) return Result.Fail($"User with ID {request.UserId} not found");
 
-                await _unitOfWork.MentionRepository.DeleteAsync(user.Mention.Id, cancellationToken);
-
-                var mention = new UserMention
+                if (user == null)
                 {
-                    Id = request.UserId,
-                    Shortname = request.Shortname,
-                    UserId = user.Id
-                };
+                    _logger.LogDebug("Update shortname failed - user not found");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail($"User with id {request.UserId} not found");
+                }
 
-                await _unitOfWork.MentionRepository.AddAsync(mention, cancellationToken);
+                if (!user.IsVerified)
+                {
+                    _logger.LogDebug("Update shortname failed - user not verified");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail("User should pass full registration");
+                }
 
+                if (user.IsDeleted)
+                {
+                    _logger.LogDebug("Update shortname failed - user is deleted");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail("User is deleted");
+                }
+
+                if (request.Shortname == user.Mention.Shortname)
+                {
+                    _logger.LogDebug("Update shortname failed - same shortname");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail("Shortname is yours");
+                }
+
+                if (await _unitOfWork.MentionRepository.ExistsByShortnameAsync(request.Shortname, cancellationToken))
+                {
+                    _logger.LogDebug("Update shortname failed - shortname is not free");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail("Shortname has been taken");
+                }
+
+                _logger.LogInformation("Shortname validated successfully, update mention: {MentionId}", user.Mention.Id);
+
+                var mention = user.Mention;
+
+                mention.Shortname = request.Shortname;
+
+                _logger.LogDebug("Saving changes to database");
+                await _unitOfWork.MentionRepository.UpdateAsync(mention, cancellationToken);
                 await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                _logger.LogDebug("Committing transaction");
                 await _unitOfWork.CommitTransactionAsync(cancellationToken);
+
+                _logger.LogInformation("Updating shortname completed successfully for user {UserId}", user.Id);
 
                 return Result.Ok();
             }
-            catch
+            catch (Exception ex)
             {
-                await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                _logger.LogError(ex, "Update shortname failed");
+
+                try
+                {
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    _logger.LogDebug("Transaction rolled back successfully");
+                }
+                catch (Exception rollbackEx)
+                {
+                    _logger.LogError(rollbackEx, "Failed to rollback transaction during shortname updating");
+                }
+
                 throw;
             }
         }
--- Application/CQRS/Users/Commands/UpdateUser.cs
+++ Application/CQRS/Users/Commands/UpdateUser.cs
@@ -4,6 +4,7 @@ using Domain.Entities;
 using FluentResults;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,20 +31,34 @@ namespace Application.CQRS.Users.Commands.UpdateUser
 
             RuleFor(x => x.UserId)
                 .NotEmpty()
-                .MustAsync(BeExist)
-                .WithMessage("User not found")
-                .MustAsync(BeVerified)
-                .WithMessage("User do not pass registration");
+                    .WithMessage("UserId is required");
+                //.MustAsync(BeExist)
+                //.WithMessage("User not found")
+                //.MustAsync(BeVerified)
+                //.WithMessage("User do not pass registration");
 
             RuleFor(x => x.Firstname)
                 .MinimumLength(UserConstants.FirstnameMinLength)
-                .MaximumLength(UserConstants.FirstnameMaxLength);
+                    .WithMessage($"Firstname's length should have minimum {UserConstants.FirstnameMinLength} characters")
+                .MaximumLength(UserConstants.FirstnameMaxLength)
+                    .WithMessage($"Firstname's length cann't have characters greater than {UserConstants.FirstnameMaxLength}");
 
             RuleFor(x => x.Lastname)
-                .MaximumLength(UserConstants.LastnameLength);
+                .MaximumLength(UserConstants.LastnameLength)
+                    .WithMessage($"Lastname's length cann't have characters greater than {UserConstants.LastnameLength}");
 
             RuleFor(x => x.Bio)
-                .MaximumLength(UserConstants.BioLength);
+                .MaximumLength(UserConstants.BioLength)
+                    .WithMessage($"Bio's length cann't have characters greater than {UserConstants.BioLength}");
+
+            RuleFor(x => x)
+                .Must(BeAllPropertiesNull)
+                    .WithMessage("There aren't any parameters");
+        }
+
+        private bool BeAllPropertiesNull(UpdateUserCommand command)
+        {
+            return command.Firstname == null && command.Lastname == null && command.Bio == null;
         }
 
         private async Task<bool> BeExist(Guid userId, CancellationToken cancellationToken)
@@ -61,36 +76,85 @@ namespace Application.CQRS.Users.Commands.UpdateUser
     public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, Result>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<UpdateUserCommandHandler> _logger;
 
         public UpdateUserCommandHandler(
-            IUnitOfWork unitOfWork)
+            IUnitOfWork unitOfWork,
+            ILogger<UpdateUserCommandHandler> logger)
         {
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
 
         public async Task<Result> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Starting update user process");
+
             await _unitOfWork.BeginTransactionAsync(cancellationToken);
 
             try
             {
+                _logger.LogDebug("Retrieving user by id {UserId} from database", request.UserId);
                 var user = await _unitOfWork.UserRepository.GetByIdAsync(request.UserId, cancellationToken);
-                if (user == null) return Result.Fail($"User with ID {request.UserId} not found");
 
+                if (user == null)
+                {
+                    _logger.LogDebug("Update user failed - user not found");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail($"User with id {request.UserId} not found");
+                }
+
+                if (!user.IsVerified)
+                {
+                    _logger.LogDebug("Update user failed - user not verified");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail("User should pass full registration");
+                }
+
+                if (user.IsDeleted)
+                {
+                    _logger.LogDebug("Update user failed - user is deleted");
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    return Result.Fail("User is deleted");
+                }
+
+                _logger.LogInformation("User validated successfully, update user: {UserId}", user.Id);
+
+                _logger.LogDebug("Update user status: {UpdateStatus}", new
+                {
+                    FirstnameChange = request.Firstname != null,
+                    LastnameChange = request.Lastname != null,
+                    BioChange = request.Bio != null,
+                });
                 user.Firstname = request.Firstname ?? user.Firstname;
                 user.Lastname = request.Lastname ?? user.Lastname;
                 user.Bio = request.Bio ?? user.Bio;
-                
+
+                _logger.LogDebug("Saving changes to database");
                 await _unitOfWork.UserRepository.UpdateAsync(user, cancellationToken);
                 await _unitOfWork.SaveChangesAsync(cancellationToken);
 
+                _logger.LogDebug("Committing transaction");
                 await _unitOfWork.CommitTransactionAsync(cancellationToken);
+
+                _logger.LogInformation("Updating user data completed successfully for user {UserId}", user.Id);
 
                 return Result.Ok();
             }
-            catch
+            catch (Exception ex)
             {
-                await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                _logger.LogError(ex, "Update user failed");
+
+                try
+                {
+                    await _unitOfWork.RollbackTransactionAsync(cancellationToken);
+                    _logger.LogDebug("Transaction rolled back successfully");
+                }
+                catch (Exception rollbackEx)
+                {
+                    _logger.LogError(rollbackEx, "Failed to rollback transaction during user updating");
+                }
+
                 throw;
             }
         }
