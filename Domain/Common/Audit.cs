7bae2059c0972c08f01bd103de8e02dfed5b92c1
IbadDesktop <kronosxplay243@gmail.com>
2025-08-28 16:09:59 +0400

    add(domain): audit interfaces

---
+++ Domain/Common/Audit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public interface ISoftDeletable
+    {
+        bool IsDeleted { get; set; }
+    }
+
+    public interface IHasCreationTime
+    {
+        DateTime CreatedAtUtc { get; set; }
+    }
+
+    public interface IHasModificationTime
+    {
+        DateTime? UpdatedAtUtc { get; set; }
+    }
+}
